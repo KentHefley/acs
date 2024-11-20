334334b54199a9431f345b36c3ef1ce5e971d81e
KentHefley <hefleykent@gmail.com>
2024-11-19 20:59:33 -0600

    Fixed site title in root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ import Footer from "./components/Footer";
 
 
 export const metadata: Metadata = {
-  title: "American Cart Service",
+  title: "America's Cart Service",
   description: "American Cart Service specializes in the recovery of stolen and misplaced shopping carts for retail businesses, helping companies reduce losses and improve operational efficiency.",
   keywords: ["shopping cart recovery", "shopping cart retreival", "stolen cart recovery", "misplaced shopping carts", "retail cart services", "cart retrieval service", "Arizona shopping cart recovery", "Texas shopping cart recovery", "lost shopping carts", "cart recovery solutions", "American Cart Service"],
 };
