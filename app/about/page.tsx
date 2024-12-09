bb769a4c7223770c9bc2fadb30cfa2f18c3f7613
KentHefley <hefleykent@gmail.com>
2024-12-09 12:02:15 -0600

    changed max width of about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Image from 'next/image'
 import { urlFor, client } from '../lib/sanity'
 import { Card, CardContent } from '@/components/ui/card'
-import {Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious,} from "@/components/ui/carousel"
+import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious, } from "@/components/ui/carousel"
 import { CarouselImages } from '../lib/interface'
 import { Button } from '@/components/ui/button'
 import Link from 'next/link'
@@ -10,28 +10,28 @@ import Link from 'next/link'
 export const revalidate = 30 // invalidate every 30 seconds
 
 
-  async function getImages():Promise<CarouselImages[]> {
-    const query = `
+async function getImages(): Promise<CarouselImages[]> {
+  const query = `
     *[_type == "carouselImage"] {
     image,
       name,
       alt,
   }
     `;
-    const images = await client.fetch(query);
-    return images;
-  }
+  const images = await client.fetch(query);
+  return images;
+}
 
 export default async function About() {
 
-const images: CarouselImages[]  = await getImages();
+  const images: CarouselImages[] = await getImages();
 
   return (
-    <div className="container max-w-5xl mx-auto mt-8 px-4 md:px-6">
-        <h1 className="text-3xl md:text-4xl text-center font-bold mb-8">About Us</h1>
-        <section className="mb-12">
-            <h2 className='text-2xl mb-4 font-semibold'>Our History</h2>
-            <p className="mb-4">
+    <div className="container max-w-4xl mx-auto mt-8 px-4 md:px-6">
+      <h1 className="text-3xl md:text-4xl text-center font-bold mb-8">About Us</h1>
+      <section className="mb-12">
+        <h2 className='text-2xl mb-4 font-semibold'>Our History</h2>
+        <p className="mb-4">
           Founded in 2003, our company began with a simple mission: to help retailers recover lost shopping carts and reduce their operational costs. What started as a small team with a handful of clients has grown into a nationwide service, trusted by major retail chains across the country.
         </p>
         <p className="mb-4">
@@ -43,31 +43,31 @@ const images: CarouselImages[]  = await getImages();
         <p>
           Today, we're proud to be at the forefront of shopping cart recovery and management. Our team of dedicated professionals, coupled with our cutting-edge technology, including daily GPS tracking of every cart in your fleet, continues to set the industry standard for efficiency and customer satisfaction.
         </p>
-        </section>
-        <section className="mb-8 mx-10">
-            <Carousel className="w-full max-w-sm mx-auto">
-                <CarouselContent>
-                    {images.map((image, index) => (
-                        <CarouselItem key={index}>
-                            <Card>
-                                <CardContent className="flex aspect-square items-center justify-center p-4">
-                            <Image 
-                                src={urlFor(image.image).url()} 
-                                width={500} 
-                                height={500} 
-                                alt={image.alt}
-                                className="rounded-t-lg object-cover w-full"
-                                />
-                                </CardContent>
-                                </Card>
-                        </CarouselItem>
-                    ))}
-                </CarouselContent>
-                <CarouselPrevious />
-                <CarouselNext />
-            </Carousel>
-        </section>
-        <section className="mb-12">
+      </section>
+      <section className="mb-8 mx-10">
+        <Carousel className="w-full max-w-sm mx-auto">
+          <CarouselContent>
+            {images.map((image, index) => (
+              <CarouselItem key={index}>
+                <Card>
+                  <CardContent className="flex aspect-square items-center justify-center p-4">
+                    <Image
+                      src={urlFor(image.image).url()}
+                      width={500}
+                      height={500}
+                      alt={image.alt}
+                      className="rounded-t-lg object-cover w-full"
+                    />
+                  </CardContent>
+                </Card>
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+          <CarouselPrevious />
+          <CarouselNext />
+        </Carousel>
+      </section>
+      <section className="mb-12">
         <h2 className="text-2xl font-semibold mb-4">Our Commitment</h2>
         <p className="mb-4">
           At the heart of our business is a commitment to excellence and customer satisfaction. We understand that every cart recovered is more than just an asset returned—it's a contribution to our clients' bottom line and operational efficiency.
